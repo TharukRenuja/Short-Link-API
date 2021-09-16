c1d6fc8cded4b7475c9d5afd8097ec06e1c4d360
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-16 16:06:04 +0530

    Added main file

---
+++ app.py
@@ -0,0 +1,29 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Short-Link-API/blob/main/LICENSE
+
+from flask import Flask, redirect, request, jsonify, json
+from shortener import short
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    if request.args.get('query'):
+        query = request.args.get('query')
+    else:
+        return redirect("https://github.com/FayasNoushad/Short-Link-API")
+    results = short(query)
+    if results is not None:
+        return jsonify(results)
+    else:
+        return "Something wrong"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="0.0.0.0", port=5000)
